846333822899d2c2d0836ab6589133f00393ba05
ke.wang <ke.wang@transwarp.io>
2024-11-28 11:23:06 +0800

    camera lookat

--- qtgl/qtglrender.cpp
+++ qtgl/qtglrender.cpp
@@ -9,13 +9,18 @@
 #include <cmath>
 #include <iostream>
 
-#define PI 3.1415926
-#define TO_RADIANS(d) ((d) * (PI / 180))
-
 namespace QTGL {
 
 using Vertice = Eigen::Vector4f;
 using Vertices = Eigen::Matrix<float, Eigen::Dynamic, 4>;
+
+struct MathUtils {
+  const static float PI;
+  inline static float toRadians(float d) { return d * (PI / 180); }
+  inline static float toDegree(float r) { return r * (180 / PI); }
+};
+
+const float MathUtils::PI = 3.1415926f;
 
 struct AffineUtils {
   static Vertices affine(Vertices& vtx, Eigen::Matrix4f& mtx) { return vtx * mtx; }
@@ -108,12 +113,17 @@ class Mesh {
       for (int i = 1; i < facet.size(); ++i) {
         Vertice p1 = vertices.row(facet[i - 1]);
         Vertice p2 = vertices.row(facet[i]);
-        // std::cout << p1[0] << "," << p1[1] << "," << p2[0] << "," << p2[1] << std::endl;
         painter.drawLine(p1[0], p1[1], p2[0], p2[1]);
       }
     }
   }
 };
+
+/*
+Heading : Yaw
+Pitch : Pitch
+Roll : Roll
+*/
 class GLCamera {
  private:
   float pos_x = 0;
@@ -121,44 +131,56 @@ class GLCamera {
   float pos_z = 0;
   float heading = 0;
   float pitch = 0;
-  float yaw = 0;
+  float roll = 0;
 
  public:
-  GLCamera() : pos_x(0), pos_y(0), pos_z(0), heading(0), pitch(0), yaw(0) {}
-  GLCamera(float pos_x, float pos_y, float pos_z, float heading, float pitch, float yaw)
-      : pos_x(pos_x), pos_y(pos_y), pos_z(pos_z), heading(heading), pitch(pitch), yaw(yaw) {}
+  GLCamera() : pos_x(0), pos_y(0), pos_z(0), heading(0), pitch(0), roll(0) {}
+  GLCamera(float pos_x, float pos_y, float pos_z, float heading, float pitch, float roll)
+      : pos_x(pos_x), pos_y(pos_y), pos_z(pos_z), heading(heading), pitch(pitch), roll(roll) {}
   void setPosition(float x, float y, float z) {
     this->pos_x = x;
     this->pos_y = y;
     this->pos_z = z;
   }
-  void setPosture(float heading, float pitch, float yaw) {
+  void setPosture(float heading, float pitch, float roll) {
     this->heading = heading;
     this->pitch = pitch;
-    this->yaw = yaw;
+    this->roll = roll;
   }
   void setPosX(float x) { this->pos_x = x; }
   void setPosY(float y) { this->pos_y = y; }
   void setPosZ(float z) { this->pos_z = z; }
   void setHeading(float heading) { this->heading = heading; }
   void setPitch(float pitch) { this->pitch = pitch; }
-  void setYaw(float yaw) { this->yaw = yaw; }
+  void setRoll(float roll) { this->roll = roll; }
   float getPosX() const { return pos_x; }
   float getPosY() const { return pos_y; }
   float getPosZ() const { return pos_z; }
   float getHeading() const { return heading; }
   float getPitch() const { return pitch; }
-  float getYaw() const { return yaw; }
+  float getRool() const { return roll; }
+
+  void lookAt(float fx, float fy, float fz, float tx, float ty, float tz) {
+    // https://stackoverflow.com/a/33790309
+    Eigen::Vector3f d{tx - fx, ty - fy, tz - fz};
+    d.normalize();
+    this->pitch = asinf(-d[1]);
+    this->heading = atan2f(d[0], d[2]);
+    this->roll = 0;
+    this->setPosition(fx, fy, fz);
+  }
 
   Eigen::Matrix4f viewMatrix() {
     Eigen::Matrix4f eyeMtx = Eigen::Matrix4f::Identity();
     Vertices rotateMtx = static_cast<Vertices>(eyeMtx);
     rotateMtx = AffineUtils::rotate_y(rotateMtx, -heading);
     rotateMtx = AffineUtils::rotate_x(rotateMtx, -pitch);
-    rotateMtx = AffineUtils::rotate_z(rotateMtx, -yaw);
+    rotateMtx = AffineUtils::rotate_z(rotateMtx, -roll);
     Vertices translateMtx = static_cast<Vertices>(eyeMtx);
     translateMtx = AffineUtils::translate(translateMtx, -pos_x, -pos_y, -pos_z);
-    return static_cast<Eigen::Matrix4f>(translateMtx) * static_cast<Eigen::Matrix4f>(rotateMtx);
+    Eigen::Matrix4f viewMtx =
+        static_cast<Eigen::Matrix4f>(translateMtx) * static_cast<Eigen::Matrix4f>(rotateMtx);
+    return viewMtx;
   }
 };
 class GLProjection {
@@ -169,7 +191,7 @@ class GLProjection {
   GLProjection(float height, float width, float near, float far)
       : height(height), width(width), near(near), far(far) {}
   Eigen::Matrix4f projMatrix() {
-    float hfov = PI / 3;
+    float hfov = MathUtils::PI / 3;
     float vfov = hfov * (height / width);
     float right = tan(hfov / 2) * near;
     float left = -right;
@@ -194,12 +216,15 @@ class GLScene {
   GLCamera camera;
   GLProjection projection;
   std::vector<Mesh> objs;
+  bool showAxis;
+  std::vector<Mesh> axis;
 
  public:
   GLScene(float viewHeight = 768.0, float viewWidth = 1024.0)
       : viewHeight(viewHeight), viewWidth(viewWidth) {
     this->projection.height = viewHeight;
     this->projection.width = viewWidth;
+    addAxis();
   }
   GLCamera& getCamera() { return this->camera; }
   GLProjection& getProjection() { return this->projection; }
@@ -214,6 +239,25 @@ class GLScene {
   void setViewSize(float w, float h) {
     this->setViewWidth(w);
     this->setViewHeight(h);
+  }
+  void setShowAxis(bool b) { this->showAxis = b; }
+  void addAxis() {
+    Mesh xAxis, yAxis, zAxis;
+    xAxis.pushVertice(-1000, 0, 0);
+    xAxis.pushVertice(1000, 0, 0);
+    xAxis.addFacet({0, 1});
+    yAxis.pushVertice(0, -1000, 0);
+    yAxis.pushVertice(0, 1000, 0);
+    yAxis.addFacet({0, 1});
+    zAxis.pushVertice(0, 0, -1000);
+    zAxis.pushVertice(0, 0, -500);
+    zAxis.pushVertice(0, 0, 0);
+    zAxis.pushVertice(0, 0, 500);
+    zAxis.pushVertice(0, 0, 1000);
+    zAxis.addFacet({0, 1, 2, 3, 4});
+    this->axis.push_back(std::move(xAxis));
+    this->axis.push_back(std::move(yAxis));
+    this->axis.push_back(std::move(zAxis));
   }
   void addObj(Mesh& mesh) { objs.push_back(mesh); }
   std::vector<Mesh>& getObjs() { return this->objs; }
@@ -231,17 +275,36 @@ class GLScene {
   Mesh meshToView(Mesh& mesh) {
     Mesh viewMesh;
     Vertices viewVertices = mesh.vertices;
+    // View矩阵
     viewVertices = viewVertices * camera.viewMatrix();
+    // 投影矩阵
     viewVertices = viewVertices * projection.projMatrix();
-    // 归一化
+    // w归一化
     viewVertices.array().colwise() /= viewVertices.col(viewVertices.cols() - 1).array();
-
+    // 视口变换
     viewVertices = viewVertices * viewMatrix();
     viewMesh.vertices = viewVertices;
     viewMesh.facets = mesh.facets;
     return viewMesh;
   }
   void draw(QPainter& painter) {
+    if (this->showAxis) {
+      QPen oldpen = painter.pen();
+      // x
+      painter.setPen(QPen(QColor(255, 0, 0), 2));
+      Mesh viewMesh = meshToView(axis[0]);
+      viewMesh.draw(painter);
+      // y
+      painter.setPen(QPen(QColor(0, 255, 0), 2));
+      viewMesh = meshToView(axis[1]);
+      viewMesh.draw(painter);
+      // z
+      painter.setPen(QPen(QColor(0, 0, 255), 2));
+      viewMesh = meshToView(axis[2]);
+      viewMesh.draw(painter);
+      // reset pen
+      painter.setPen(oldpen);
+    }
     for (Mesh& mesh : objs) {
       Mesh viewMesh = meshToView(mesh);
       viewMesh.draw(painter);
@@ -249,11 +312,13 @@ class GLScene {
   }
 };
 class GLRenderWidget : public QWidget {
- public:
+ private:
   GLScene scene;
+
+ public:
   GLRenderWidget(QWidget* parent = nullptr) : QWidget(parent) {
     QTimer* timer = new QTimer(this);                                  // 创建定时器
-    timer->setInterval(10);                                            // 定时间隔单位ms
+    timer->setInterval(20);                                            // 定时间隔单位ms
     connect(timer, &QTimer::timeout, this, &GLRenderWidget::refresh);  // 定时器关联refresh
     timer->start();                                                    // 定时启动
   }
@@ -264,12 +329,16 @@ class GLRenderWidget : public QWidget {
     QWidget::setFixedSize(w, h);
   }
 
+  GLScene& getScene() { return scene; }
+
   void refresh() { this->update(); }
 
   void paintEvent(QPaintEvent* event) override {
     QPainter painter(this);
     painter.eraseRect(0, 0, this->width(), this->height());  // 清除画布
-    scene.getObjs()[0].rotate_z(TO_RADIANS(1));
+    // scene.getObjs()[0].rotate_x(MathUtils::toRadians(1));
+    // scene.getObjs()[0].rotate_y(MathUtils::toRadians(1));
+    scene.getObjs()[0].rotate_z(MathUtils::toRadians(1));
     scene.draw(painter);
   }
 };
@@ -285,8 +354,9 @@ int main(int argc, char* argv[]) {
   QTGL::GLRenderWidget widget;
   widget.setFixedSize(600, 600);
   QTGL::Mesh mesh = QTGL::Mesh::makeCube(50);
-  widget.scene.getCamera().setPosition(0, 0, -300);
-  widget.scene.addObj(mesh);
+  widget.getScene().getCamera().lookAt(300, 300, 300, 0, 0, 0);
+  widget.getScene().addObj(mesh);
+  widget.getScene().setShowAxis(true);
   layout->addWidget(&widget, 0, 0);
 
   window->setLayout(layout);
